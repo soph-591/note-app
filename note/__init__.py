b831edc55978f0a50a72ee7800db74c773b5871a
soph-591 <sophiepaul591@gmail.com>
2026-02-15 09:04:27 +0000

    upload folder made relative to instance path

--- note/__init__.py
+++ note/__init__.py
@@ -1,17 +1,14 @@
 import os
 from flask import Flask
 
-UPLOAD_FOLDER='uploads/'
-
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'note.sqlite'),
     )
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-    os.makedirs(app.instance_path, exist_ok=True)
+    app.config['UPLOAD_FOLDER'] = os.path.join(app.instance_path, "uploads")
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
     from . import db
     db.init_app(app)
